c07a4049f2635e3a75819f34d62bea5a9c25b3a6
VojtasekP <vojtasek.p@gmail.com>
2025-03-11 20:53:58 +0100

    adding first hw scripts

---
+++ homeworks/hw1.py
@@ -0,0 +1,244 @@
+import os
+from typing import Any
+
+import torch
+from pytorch_lightning.utilities.types import TRAIN_DATALOADERS, EVAL_DATALOADERS
+
+torch.set_float32_matmul_precision('high')
+
+
+from escnn import gspaces
+from escnn import nn
+from tqdm import tqdm
+from tqdm.asyncio import trange
+
+
+
+from torch.utils.data import Dataset
+from torchvision.transforms import RandomRotation
+from torchvision.transforms import Pad
+from torchvision.transforms import Resize
+from torchvision.transforms import ToTensor
+from torchvision.transforms import Compose
+from torchvision.transforms import InterpolationMode
+import pytorch_lightning as pl
+import numpy as np
+
+from PIL import Image
+
+from homeworks.mnist_rot import MnistRotDataset
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# download the dataset
+
+import dataset_download
+
+class C8SteerableCNN(pl.LightningModule):
+
+    def __init__(self, n_classes=10):
+        super(C8SteerableCNN, self).__init__()
+
+        # the model is equivariant under rotations by 45 degrees, modelled by C8
+        self.r2_act = gspaces.rot2dOnR2(N=8)
+
+        # the input image is a scalar field, corresponding to the trivial representation
+        in_type = nn.FieldType(self.r2_act, [self.r2_act.trivial_repr])
+
+        # we store the input type for wrapping the images into a geometric tensor during the forward pass
+        self.input_type = in_type
+
+        # convolution 1
+        # first specify the output type of the convolutional layer
+        # we choose 24 feature fields, each transforming under the regular representation of C8
+        out_type = nn.FieldType(self.r2_act, 24 * [self.r2_act.regular_repr])
+        self.block1 = nn.SequentialModule(
+            nn.MaskModule(in_type, 29, margin=1),
+            nn.R2Conv(in_type, out_type, kernel_size=7, padding=1, bias=False),
+            nn.InnerBatchNorm(out_type),
+            nn.ReLU(out_type, inplace=True)
+        )
+
+        # convolution 2
+        # the old output type is the input type to the next layer
+        in_type = self.block1.out_type
+        # the output type of the second convolution layer are 48 regular feature fields of C8
+        out_type = nn.FieldType(self.r2_act, 48 * [self.r2_act.regular_repr])
+        self.block2 = nn.SequentialModule(
+            nn.R2Conv(in_type, out_type, kernel_size=5, padding=2, bias=False),
+            nn.InnerBatchNorm(out_type),
+            nn.ReLU(out_type, inplace=True)
+        )
+        self.pool1 = nn.SequentialModule(
+            nn.PointwiseAvgPoolAntialiased(out_type, sigma=0.66, stride=2)
+        )
+
+        # convolution 3
+        # the old output type is the input type to the next layer
+        in_type = self.block2.out_type
+        # the output type of the third convolution layer are 48 regular feature fields of C8
+        out_type = nn.FieldType(self.r2_act, 48 * [self.r2_act.regular_repr])
+        self.block3 = nn.SequentialModule(
+            nn.R2Conv(in_type, out_type, kernel_size=5, padding=2, bias=False),
+            nn.InnerBatchNorm(out_type),
+            nn.ReLU(out_type, inplace=True)
+        )
+
+        # convolution 4
+        # the old output type is the input type to the next layer
+        in_type = self.block3.out_type
+        # the output type of the fourth convolution layer are 96 regular feature fields of C8
+        out_type = nn.FieldType(self.r2_act, 96 * [self.r2_act.regular_repr])
+        self.block4 = nn.SequentialModule(
+            nn.R2Conv(in_type, out_type, kernel_size=5, padding=2, bias=False),
+            nn.InnerBatchNorm(out_type),
+            nn.ReLU(out_type, inplace=True)
+        )
+        self.pool2 = nn.SequentialModule(
+            nn.PointwiseAvgPoolAntialiased(out_type, sigma=0.66, stride=2)
+        )
+
+        # convolution 5
+        # the old output type is the input type to the next layer
+        in_type = self.block4.out_type
+        # the output type of the fifth convolution layer are 96 regular feature fields of C8
+        out_type = nn.FieldType(self.r2_act, 96 * [self.r2_act.regular_repr])
+        self.block5 = nn.SequentialModule(
+            nn.R2Conv(in_type, out_type, kernel_size=5, padding=2, bias=False),
+            nn.InnerBatchNorm(out_type),
+            nn.ReLU(out_type, inplace=True)
+        )
+
+        # convolution 6
+        # the old output type is the input type to the next layer
+        in_type = self.block5.out_type
+        # the output type of the sixth convolution layer are 64 regular feature fields of C8
+        out_type = nn.FieldType(self.r2_act, 64 * [self.r2_act.regular_repr])
+        self.block6 = nn.SequentialModule(
+            nn.R2Conv(in_type, out_type, kernel_size=5, padding=1, bias=False),
+            nn.InnerBatchNorm(out_type),
+            nn.ReLU(out_type, inplace=True)
+        )
+        self.pool3 = nn.PointwiseAvgPoolAntialiased(out_type, sigma=0.66, stride=1, padding=0)
+
+        self.gpool = nn.GroupPooling(out_type)
+
+        # number of output channels
+        c = self.gpool.out_type.size
+
+        # Fully Connected
+        self.fully_net = torch.nn.Sequential(
+            torch.nn.Linear(c, 64),
+            torch.nn.BatchNorm1d(64),
+            torch.nn.ELU(inplace=True),
+            torch.nn.Linear(64, n_classes),
+        )
+
+    def forward(self, input: torch.Tensor):
+        # wrap the input tensor in a GeometricTensor
+        # (associate it with the input type)
+        x = nn.GeometricTensor(input, self.input_type)
+
+        # apply each equivariant block
+
+        # Each layer has an input and an output type
+        # A layer takes a GeometricTensor in input.
+        # This tensor needs to be associated with the same representation of the layer's input type
+        #
+        # The Layer outputs a new GeometricTensor, associated with the layer's output type.
+        # As a result, consecutive layers need to have matching input/output types
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.pool1(x)
+
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.pool2(x)
+
+        x = self.block5(x)
+        x = self.block6(x)
+
+        # pool over the spatial dimensions
+        x = self.pool3(x)
+
+        # pool over the group
+        x = self.gpool(x)
+
+        # unwrap the output GeometricTensor
+        # (take the Pytorch tensor and discard the associated representation)
+        x = x.tensor
+
+        # classify with the final fully connected layers)
+        x = self.fully_net(x.reshape(x.shape[0], -1))
+
+        return x
+    def loss(self, input: Any, target: Any):
+        criterion = torch.nn.CrossEntropyLoss()
+        return criterion(input, target)
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = self.loss(y_hat, y)
+        self.log('train_loss', loss, prog_bar=True,logger=True)
+        return loss
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        val_loss = self.loss(y_hat, y)
+        self.log('val_loss', val_loss, prog_bar=True,logger=True)
+        return val_loss
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=5e-5, weight_decay=1e-5)
+
+    def train_dataloader(self):
+        num_workers = min(8, os.cpu_count() // 2)
+        # this allows to use odd-size filters with stride 2 when downsampling a feature map in the model
+        pad = Pad((0, 0, 1, 1), fill=0)
+        resize1 = Resize(87)
+        resize2 = Resize(29)
+        totensor = ToTensor()
+        train_transform = Compose([
+            pad,
+            resize1,
+            RandomRotation(180., interpolation=InterpolationMode.BILINEAR, expand=False),
+            resize2,
+            totensor,
+        ])
+
+        mnist_train = MnistRotDataset(mode='train', transform=train_transform)
+        train_loader = torch.utils.data.DataLoader(mnist_train, shuffle=True, batch_size=64, num_workers=num_workers)
+        return train_loader
+    def val_dataloader(self):
+        num_workers = min(8, os.cpu_count() // 2)
+        # this allows to use odd-size filters with stride 2 when downsampling a feature map in the model
+        pad = Pad((0, 0, 1, 1), fill=0)
+        totensor = ToTensor()
+        test_transform = Compose([
+            pad,
+            totensor,
+        ])
+        mnist_val = MnistRotDataset(mode='test', transform=test_transform)
+
+        val_loader = torch.utils.data.DataLoader(mnist_val, batch_size=64, num_workers=num_workers)
+        return val_loader
+def test(model, test_loader):
+    total = 0
+    correct = 0
+    model.eval()
+    with torch.no_grad():
+        for i, (data, targets) in enumerate(test_loader):
+            data = data.to(device)
+            targets = targets.to(device)
+
+            y = model(data)
+
+            _, prediction = torch.max(y.data, 1)
+            total += targets.shape[0]
+            correct += (prediction == targets).sum().item()
+
+
+
+if __name__ == '__main__':
+
+    trainer = pl.Trainer(accelerator="gpu",devices=1)
+    g_cnn = C8SteerableCNN().to(device)
+    trainer.fit(g_cnn)
---
+++ homeworks/visualize_mnist.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+from torchvision.transforms import RandomRotation
+from torchvision.transforms import Pad
+from torchvision.transforms import Resize
+from torchvision.transforms import ToTensor
+from torchvision.transforms import Compose
+from torchvision.transforms import InterpolationMode
+from homeworks.mnist_rot import MnistRotDataset
+import random
+import keyboard  # Install with `pip install keyboard`
+
+# load mnist dataset
+pad = Pad((0, 0, 1, 1), fill=0)
+
+# to reduce interpolation artifacts (e.g. when testing the model on rotated images),
+# we upsample an image by a factor of 3, rotate it and finally downsample it again
+resize1 = Resize(87)
+resize2 = Resize(29)
+
+totensor = ToTensor()
+train_transform = Compose([
+    pad,
+    resize1,
+    RandomRotation(180., interpolation=InterpolationMode.BILINEAR, expand=False),
+    resize2,
+    totensor,
+])
+
+mnist_train = MnistRotDataset(mode='train', transform=train_transform)
+def show_random_image():
+    idx = random.randint(0, len(mnist_train) - 1)
+    image, label = mnist_train[idx]
+
+    plt.imshow(image.squeeze(), cmap="gray")
+    plt.title(f"Label: {label}")
+    plt.show()
+
+while True:
+    show_random_image()
+    user_input = input("Press q to continue").strip().lower()
+    if user_input != "q":
+        break
